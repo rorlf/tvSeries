c65565ae58ff008e1e61ad1bdd424b406bc55445
Rodrigo <rodrigo@gtacsolutions.com>
2022-02-20 17:24:57 -0300

    feat(navigator): improve style of tabs in HomeNavigator

--- src/navigators/HomeNavigator/styles.ts
+++ src/navigators/HomeNavigator/styles.ts
@@ -2,11 +2,13 @@ import { StyleSheet } from 'react-native';
 import { useTheme } from 'store/slices/themeSlice';
 
 export default function useStyles() {
-  const { colors } = useTheme();
+  const { colors, metrics } = useTheme();
 
   return StyleSheet.create({
     tabBar: {
       backgroundColor: colors.navigationBar,
+      height: 52,
+      paddingBottom: metrics.spacing(1),
     },
   });
 }
